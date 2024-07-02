e05b745cd371f14398b6fd25f23c7e810bf9b055
Will Skelly <60189821+wskel@users.noreply.github.com>
2024-07-01 22:00:50 -0400

    Add SQL Server connection string configuration

--- MediaLendingService.Server/Program.cs
+++ MediaLendingService.Server/Program.cs
@@ -1,18 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DbContext>(optionsBuilder =>
+    optionsBuilder.UseSqlServer(connectionString)
+);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<DbContext>();
+    dbContext.Database.Migrate();
+}
+
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
