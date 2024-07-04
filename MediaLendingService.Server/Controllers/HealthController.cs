2242ce974c6eb6360f1812d5b6565ffb19c5acd1
Will Skelly <60189821+wskel@users.noreply.github.com>
2024-07-04 00:52:38 -0400

    Add health check endpoint

---
+++ MediaLendingService.Server/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaLendingService.Server.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [AllowAnonymous]
+    [HttpGet]
+    public ActionResult HealthCheck() => Ok();
+}
